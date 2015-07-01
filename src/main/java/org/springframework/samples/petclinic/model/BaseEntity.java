d7b7fe8583cf6635e2becabd8e644d3f5ed1c741
mikalai_shchurko <mikalai_shchurko@epam.com>
2015-07-01 15:39:33 +0300

    Adding dummy comment

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -22,7 +22,7 @@ import javax.persistence.MappedSuperclass;
 
 /**
  * Simple JavaBean domain object with an id property. Used as a base class for objects needing this property.
- *
+ * dummy comment
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
